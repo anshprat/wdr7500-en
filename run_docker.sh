a7c910aa23e22486eb1b4561829f2ccefcaef7cb
Anshu Prateek <333902+anshprat@users.noreply.github.com>
2021-05-01 20:35:07 +0530

    fix optional param

--- run_docker.sh
+++ run_docker.sh
@@ -1,7 +1,7 @@
-set +x
+set -x
 docker_name=proxy
 docker rm -f ${docker_name}
-mac_interface=${1:en0}
+mac_interface=${1:-en0}
 mac_local_ip=`ifconfig ${mac_interface} inet|grep inet|awk '{print $2}'`
 #mac sed
 sed -e "s/LOCAL_IP/${mac_local_ip}/g" proxypass.conf.template >proxypass.conf
